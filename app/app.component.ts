3070ca2e645687aabcfe516a81e0b5879995cb9b
Cristian Muscalu <muscaiu@yahoo.com>
2016-06-10 16:20:23 +0300

    added home, posts, users components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,21 @@
 import {Component} from '@angular/core';
 
 import {NavbarComponent} from './navbar.component';
+import {HomeComponent} from './home/home.component';
+import {UsersComponent} from './users/users.component';
+import {PostsComponent} from './posts/posts.component';
+
+
 
 @Component({
     selector: 'my-app',
     template: '<navbar></navbar>',
-    directives: [NavbarComponent] 
+    directives: [
+        NavbarComponent,
+        HomeComponent,
+        UsersComponent,
+        PostsComponent
+        ] 
 })
 
 export class AppComponent{
---
+++ app/posts/posts.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    template: '<h2>Posts<h2>'
+})
+
+export class PostsComponent{
+    
+}
