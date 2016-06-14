ca4c8d8183322d6a208cd819a8450fa2331a462f
Cristian Muscalu <muscaiu@yahoo.com>
2016-06-14 13:55:04 +0300

    added spinner component for users list

--- app/users/users.component.ts
+++ app/users/users.component.ts
@@ -1,26 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from './user.service';
+import {SpinnerComponent} from '../shared/spinner.component';
 
 @Component({
     template: `
         <h2>Users<h2>
+        <spinner [visible]="loadingUsers"></spinner>
         <ul>
             <li *ngFor="let u of users">
                 {{u.name}}
             </li>
         </ul>
     `,
-    providers:[UserService]
+    providers:[UserService],
+    directives: [SpinnerComponent]
 })
 
 export class UsersComponent implements OnInit{
-    users:any;
+    users:any; 
+    loadingUsers = true;
 
     constructor(private _userService:UserService){
     }
     
     ngOnInit(){
         this._userService.getUsers()
-            .subscribe(users => this.users = users);
+            .subscribe(users => {
+                this.users = users;
+                this.loadingUsers = false;
+            });
     }
 }
